d43e2e972bf22db4e29380a3565b366aae76691c
huyqt97 <huyth290197@gmail.com>
2023-09-07 23:11:14 +0700

    TH

---
+++ src/main/java/ra/Md5Ss5ThApplication.java
@@ -0,0 +1,13 @@
+package ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md5Ss5ThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Md5Ss5ThApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ra/Md5Ss5ThApplicationTests.java
@@ -0,0 +1,13 @@
+package ra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md5Ss5ThApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
